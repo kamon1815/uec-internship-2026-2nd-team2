ce4064c2562257812079b8f966fb7243ed5b4811
UEC6 <pj_internship_UEC6@photron.co.jp>
2026-09-08 16:37:26 +0900

    コード判定
    コード判定の結果を表示できるようになりました(webカメラ)

--- shisaku/pattern matching.py
+++ shisaku/pattern matching.py
@@ -2,6 +2,7 @@ import time
 import cv2
 import numpy as np
 import openvino as ov
+# import pose_estimation
 from pathlib import Path
 from pypuclib import CameraFactory
 
@@ -34,87 +35,85 @@ print(f"適用されたWebカメラ解像度: {W}x{H}, FPS: {fps}")
 
 # モデル
 core = ov.Core()
-print(f"使えるデバイス: {core.available_devices}")
 
 # cam = CameraFactory().create() # INFINICAM
 # cam.setFramerateShutter(30, 30)
 # decoder = cam.decoder()
 # W, H = cam.resolution().width, cam.resolution().height
 # USE_GPU_DECODE = decoder.getAvailableGPUProcess()     # CUDA専用。無ければCPUデコード
-img = cap
+#ret, img = cap.read()
 BASE_DIR = Path(__file__).resolve().parent
+# コードファイルの読み取り
+def get_codetype(img):
+    globals
+    code_A = BASE_DIR / "codetype/1.jpeg"
+    code_D = BASE_DIR / "codetype/2.png"
+    code_E = BASE_DIR / "codetype/3.png"
 
-code_A = BASE_DIR / "codetype/コードA.jpg"
-code_D = BASE_DIR / "codetype/コードD.jpg"
-code_E = BASE_DIR / "codetype/コードE.jpg"
+    code_A_1 = cv2.imread(code_A)
+    code_D_1 = cv2.imread(code_D)
+    code_E_1 = cv2.imread(code_E)
+    if code_A_1 is None:
+        print("エラー: 画像ファイルが見つからないか、読み込めませんでした。")
+    
+    # グレースケール
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    code_A_gray = cv2.cvtColor(code_A_1, cv2.COLOR_BGR2GRAY)
+    code_D_gray = cv2.cvtColor(code_D_1, cv2.COLOR_BGR2GRAY)
+    code_E_gray = cv2.cvtColor(code_E_1, cv2.COLOR_BGR2GRAY)
+    #print(img_gray)
+    # テンプレートマッチングの実行
+    result_A = cv2.matchTemplate(img_gray, code_A_gray, cv2.TM_CCOEFF_NORMED)
+    result_D = cv2.matchTemplate(img_gray, code_D_gray, cv2.TM_CCOEFF_NORMED)
+    result_E = cv2.matchTemplate(img_gray, code_E_gray, cv2.TM_CCOEFF_NORMED)
 
-if code_A is None:
-    print("エラー: 画像ファイルが見つからないか、読み込めませんでした。パスを確認してください。")
-else:
-    print("画像の読み込みに成功しました。")
+    # テンプレートの最大類似度と位置
+    # min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+    _, max_val_A, _, max_loc_A = cv2.minMaxLoc(result_A)
+    _, max_val_D, _, max_loc_D = cv2.minMaxLoc(result_D)
+    _, max_val_E, _, max_loc_E = cv2.minMaxLoc(result_E)
 
-# グレースケール
-img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-code_A_gray = cv2.cvtColor(code_A, cv2.COLOR_BGR2GRAY)
-code_D_gray = cv2.cvtColor(code_D, cv2.COLOR_BGR2GRAY)
-code_E_gray = cv2.cvtColor(code_E, cv2.COLOR_BGR2GRAY)
+    # 一番スコアが高いものを表示
+    results = {
+        'コードA': (max_val_A, max_loc_A, code_A_gray.shape),
+        'コードD': (max_val_D, max_loc_D, code_D_gray.shape),
+        'コードE': (max_val_E, max_loc_E, code_E_gray.shape)
+        }
+    
 
-# テンプレートマッチングの実行
-result_A = cv2.matchTemplate(img_gray, code_A_gray, cv2.TM_CCOEFF_NORMED)
-result_D = cv2.matchTemplate(img_gray, code_D_gray, cv2.TM_CCOEFF_NORMED)
-result_E = cv2.matchTemplate(img_gray, code_E_gray, cv2.TM_CCOEFF_NORMED)
 
-# テンプレートの最大類似度と位置
-# min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
-_, max_val_A, _, max_loc_A = cv2.minMaxLoc(result_A)
-_, max_val_D, _, max_loc_D = cv2.minMaxLoc(result_D)
-_, max_val_E, _, max_loc_E = cv2.minMaxLoc(result_E)
-
-# 一番スコアが高いものを表示
-results = {
-    'コードA': (max_val_A, max_loc_A, code_A_gray.shape),
-    'コードD': (max_val_D, max_loc_D, code_D_gray.shape),
-    'コードE': (max_val_E, max_loc_E, code_E_gray.shape)
-}
-
-# 描画する。
-dst = img.copy()
-for x, y in zip():
-    cv2.rectangle(
-        dst,
-        (x, y),
-        (x + code_A.shape[1], y + code_A.shape[0]),
-        color=(0, 255, 0),
-        thickness=2,
-    )
+    # 描画する。
+    dst = img.copy()
+    for x, y in zip():
+        cv2.rectangle(
+            dst,
+            (x, y),
+            (x + code_A.shape[1], y + code_A.shape[0]),
+            color=(0, 255, 0),
+            thickness=2,
+        )
+    if (max_val_A > max_val_D) & (max_val_A > max_val_E):
+        print('コードA')
+    elif max_val_D > max_val_E:
+        print('コードD')
+    else:
+        print('コードE')
+    return code_A_1, code_D_1, code_E_1
 
 
 colorize = True
 fps, t_prev = 0.0, time.perf_counter()
 
 while True:
-    ret, gray = cap.read()
-    if not ret:
-            print("カメラからのフレーム取得に失敗しました。")
-            break
-    if gray.ndim == 3:                          # 1chに揃える
-            gray = gray[:, :, 0]
-    h, w = gray.shape[:2]
+    # ウェブカメラの画像取得
+    ret, img = cap.read()
+    code_A_1, code_D_1, code_E_1 = get_codetype(img)
 
-    frame = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)
+    # 画像表示
+    cv2.imshow('test', img)
+    key = cv2.waitKey(1)
+    if key & 0xFF == 27: # Esc : quit application
+               break
 
-    # 表示
-    now = time.perf_counter()
-    fps = 0.9 * fps + 0.1 / (now - t_prev)      # 移動平均でちらつきを抑える
-    t_prev = now
-
-    cv2.putText(frame, f"{MODE}  {fps:5.1f} fps", (10, 30),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2)
-    cv2.imshow("INFINICAM", frame)
-
-    key = cv2.waitKey(1) & 0xFF
-    if key == 27:                                # Esc:終了
-        break
-
-cv2.destroyAllWindows()
 cap.release()
+cv2.destroyAllWindows()
