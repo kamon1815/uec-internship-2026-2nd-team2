a7a77d6a14593c111e38d200ff62b5ad823f53c5
UEC6 <pj_internship_UEC6@photron.co.jp>
2026-09-09 12:11:06 +0900

    座標をよみとるようにしているところです

--- shisaku/pattern matching.py
+++ shisaku/pattern matching.py
@@ -2,53 +2,53 @@ import time
 import cv2
 import numpy as np
 import openvino as ov
-# import pose_estimation
+import main
 from pathlib import Path
 from pypuclib import CameraFactory
 
-MODE = "npu_fp16"   
+# MODE = "npu_fp16"   
 
-DEVICE, PRECISION = {"cpu_fp32": ("CPU", "f32"),
-                     "gpu_fp16": ("GPU", "f16"),
-                     "npu_fp16": ("NPU", "f16")}[MODE]
-# 画像の読み込み
-# カメラ
-cap = cv2.VideoCapture(0) # webカメラ
+# DEVICE, PRECISION = {"cpu_fp32": ("CPU", "f32"),
+#                      "gpu_fp16": ("GPU", "f16"),
+#                      "npu_fp16": ("NPU", "f16")}[MODE]
+# # 画像の読み込み
+# # カメラ
+# cap = cv2.VideoCapture(0) # webカメラ
 
-if not cap.isOpened():
-    print("エラー: Webカメラを開けませんでした。")
-    exit()
+# if not cap.isOpened():
+#     print("エラー: Webカメラを開けませんでした。")
+#     exit()
 
-# カメラの最大解像度を要求
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 10000)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 10000)
+# # カメラの最大解像度を要求
+# cap.set(cv2.CAP_PROP_FRAME_WIDTH, 10000)
+# cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 10000)
 
-# 解像度 W, H および FPS を取得
-W = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-H = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-fps = cap.get(cv2.CAP_PROP_FPS)
+# # 解像度 W, H および FPS を取得
+# W = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+# H = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+# fps = cap.get(cv2.CAP_PROP_FPS)
 
-if fps == 0 or fps is None or fps > 120:
-    fps = 30.0
+# if fps == 0 or fps is None or fps > 120:
+#     fps = 30.0
 
-print(f"適用されたWebカメラ解像度: {W}x{H}, FPS: {fps}")
+# print(f"適用されたWebカメラ解像度: {W}x{H}, FPS: {fps}")
 
-# モデル
-core = ov.Core()
+# # モデル
+# core = ov.Core()
 
 # cam = CameraFactory().create() # INFINICAM
 # cam.setFramerateShutter(30, 30)
 # decoder = cam.decoder()
 # W, H = cam.resolution().width, cam.resolution().height
 # USE_GPU_DECODE = decoder.getAvailableGPUProcess()     # CUDA専用。無ければCPUデコード
-#ret, img = cap.read()
+# #ret, img = cap.read()
 BASE_DIR = Path(__file__).resolve().parent
 # コードファイルの読み取り
 def get_codetype(img):
     globals
-    code_A = BASE_DIR / "codetype/1.jpeg"
-    code_D = BASE_DIR / "codetype/2.png"
-    code_E = BASE_DIR / "codetype/3.png"
+    code_A = BASE_DIR / "codetype/1.png"
+    code_D = BASE_DIR / "codetype/2.jpg"
+    code_E = BASE_DIR / "codetype/3.jpg"
 
     code_A_1 = cv2.imread(code_A)
     code_D_1 = cv2.imread(code_D)
@@ -61,7 +61,7 @@ def get_codetype(img):
     code_A_gray = cv2.cvtColor(code_A_1, cv2.COLOR_BGR2GRAY)
     code_D_gray = cv2.cvtColor(code_D_1, cv2.COLOR_BGR2GRAY)
     code_E_gray = cv2.cvtColor(code_E_1, cv2.COLOR_BGR2GRAY)
-    #print(img_gray)
+
     # テンプレートマッチングの実行
     result_A = cv2.matchTemplate(img_gray, code_A_gray, cv2.TM_CCOEFF_NORMED)
     result_D = cv2.matchTemplate(img_gray, code_D_gray, cv2.TM_CCOEFF_NORMED)
@@ -79,7 +79,7 @@ def get_codetype(img):
         'コードD': (max_val_D, max_loc_D, code_D_gray.shape),
         'コードE': (max_val_E, max_loc_E, code_E_gray.shape)
         }
-    
+
 
 
     # 描画する。
@@ -92,28 +92,42 @@ def get_codetype(img):
             color=(0, 255, 0),
             thickness=2,
         )
-    if (max_val_A > max_val_D) & (max_val_A > max_val_E):
+    if max_val_A > 0.7:
         print('コードA')
-    elif max_val_D > max_val_E:
+    elif max_val_D > 0.7:
         print('コードD')
-    else:
+    elif max_val_E > 0.7:
         print('コードE')
     return code_A_1, code_D_1, code_E_1
 
 
-colorize = True
-fps, t_prev = 0.0, time.perf_counter()
+lx1 = None
+ly1 = None
+lx2 = None
+ly2 = None
+lx3 = None
+ly3 = None
+
+# 座標からコード判定
+def decide_code():
+    # 座標取得
+    finger = main.get_finger_position('Left', 1)
+    global lx1, ly1, lx2, ly2, lx3, ly3
+    lx1 = finger[3].x
+    ly1 = finger[3].y
+
+    finger = main.get_finger_position('Left', 2)
+    
+    lx2 = finger[3].x
+    ly2 = finger[3].y
+
+    finger = main.get_finger_position('Left', 3)
+    
+    lx3 = finger[3].x
+    ly3 = finger[3].y
+    print(lx1, ly1, lx2, ly2, lx3, ly3)
+    return(lx1, ly1, lx2, ly2, lx3, ly3)
+
 
 while True:
-    # ウェブカメラの画像取得
-    ret, img = cap.read()
-    code_A_1, code_D_1, code_E_1 = get_codetype(img)
-
-    # 画像表示
-    cv2.imshow('test', img)
-    key = cv2.waitKey(1)
-    if key & 0xFF == 27: # Esc : quit application
-               break
-
-cap.release()
-cv2.destroyAllWindows()
+    lx1, ly1, lx2, ly2, lx3, ly3 = decide_code()
