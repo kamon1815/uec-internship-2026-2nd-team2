b6293a7bfbbc134aa90665cc7008d01e877f0999
UEC6 <pj_internship_UEC6@photron.co.jp>
2026-09-10 13:39:34 +0900

    判定基準いい感じ

--- shisaku/main_gui.py
+++ shisaku/main_gui.py
@@ -659,7 +659,7 @@ def decide_code(lx1, ly1, lx2, ly2, lx3, ly3):
     # 三角形の面積
     area = abs(cross_product) / 2
     print(area)
-    judge = 0.00002
+    judge = 0.00005
     # if 0.15 < dist_31:
     #     print("コードA")
     #     return("ラ")
