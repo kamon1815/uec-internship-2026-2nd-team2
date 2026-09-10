a36ab09c068e24a92310f36821c9b39472dfae49
UEC6 <pj_internship_UEC6@photron.co.jp>
2026-09-10 11:32:00 +0900

    3つのコードを判定可能
    簡単な条件ならできるようになりました

--- shisaku/main_gui.py
+++ shisaku/main_gui.py
@@ -191,9 +191,10 @@ class Application(tk.Frame):
         # 相対座標の取得
         global was_on_guitar
         relative_rx, relative_ry, is_on_guitar = get_hand_relative_position()
-        if (was_on_guitar == 0) and (is_on_guitar == 1):
+        if (was_on_guitar == 0) and (is_on_guitar == 1) and (get_hands_count(current_hands) == 2):
             print("再生中")
-            chord = get_chord_by_position_l(100, 150) # 変数化
+            lx1, ly1, lx2, ly2, lx3, ly3 = get_lefthand_potions()
+            chord = decide_code(lx1, ly1, lx2, ly2, lx3, ly3)
             self.start_sound(chord)
         #else:
             #print("再生条件を満たしていません")
@@ -606,28 +607,6 @@ def draw_start_position(img):
     else:
         return img
 
-def get_chord_by_position_l(xl1, yl1):
-    # テスト (座標)
-    base_lx = 100 # 変数化
-    base_ly = 200
-    current_lx = 207
-    current_ly = 290
-    relative_lx = current_lx - base_lx
-    relative_ly = current_ly - base_ly
-    # 指の座標でコードを決める
-    if (0 <= relative_lx < 200) & (0 <= relative_ly < 200): # 範囲の決定
-        chord_type = "ラ"
-    elif (200 <= relative_lx < 400) & (200 <= relative_ly < 400):
-        chord_type = "レ"
-    elif (400 <= relative_lx < 600) & (400 <= relative_ly < 600):
-        chord_type = "ミ"
-    else:
-        print('コードがわかりません')
-        print(f"左手の相対座標:({relative_lx}, {relative_ly})")
-        return 
-
-    print(f"コード{chord_type}")
-    return chord_type
 
 def get_hand_relative_position(): #基準点(base_rx,base_ry)に対する現在の指の相対位置を取得
     hand_position = get_hand_position('Right', current_hands)
